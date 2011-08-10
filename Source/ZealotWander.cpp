f31665439c6a8a9e5036e8686a1ba3d196491fa3
Geoff Shannon <earthlingzephyr@gmail.com>
2011-08-10 02:47:59 -0700

    More updates to zealot
    Signed-off-by: Geoff Shannon <earthlingzephyr@gmail.com>

--- Source/ZealotWander.cpp
+++ Source/ZealotWander.cpp
@@ -2,11 +2,22 @@
 #include <random>
 
 void ZealotWander::onFrame(void) {
-	if (!m_unit->isVisible()) {
+	if (!m_unit->getHitPoints() < 1) {
 		BWAPI::Broodwar->printf("Deleting zealot wanderer");
 		delete this;
-	} else if (m_unit->isAttacking() || m_unit->isStartingAttack()) {
-		;
+		return;
+	}
+	
+	if (m_attacking && !m_unit->isIdle()) {
+		return;
+	} else if (m_unit->isIdle()) {
+		m_attacking = false;
+	}
+
+	BWAPI::Unit *enemy = nearestEnemy();
+	if (enemy) {
+		m_unit->attack(enemy);
+		m_attacking = true;
 	} else if (m_unit->isIdle()) {
 		m_visitedPositions.insert(m_unit->getPosition());
 		m_unit->attack(newDestination());
@@ -14,12 +25,18 @@ void ZealotWander::onFrame(void) {
 }
 
 BWAPI::Position ZealotWander::newDestination(void) {
-	BWAPI::Position newPos(std::rand() % BWAPI::Broodwar->mapWidth(),
-						   std::rand() % BWAPI::Broodwar->mapHeight());
+	BWAPI::Position newPos(std::rand() % (BWAPI::Broodwar->mapWidth() * TILE_SIZE),
+						   std::rand() % (BWAPI::Broodwar->mapHeight() * TILE_SIZE));
 
 	while (m_visitedPositions.find(newPos) != m_visitedPositions.end() && BWAPI::Broodwar->hasPath(m_unit->getPosition(), newPos)) {
 		newPos = BWAPI::Position(std::rand() % BWAPI::Broodwar->mapWidth(),
 								 std::rand() % BWAPI::Broodwar->mapHeight());
 	}
 	return newPos;
+}
+
+BWAPI::Unit *ZealotWander::nearestEnemy(void) {
+	UnitSet units = BWAPI::Broodwar->enemy()->getUnits();
+	BWAPI::Broodwar->printf("number of enemy units: %d", units.size());
+	return 0;
 }
--- Source/ZealotWander.h
+++ Source/ZealotWander.h
@@ -6,7 +6,7 @@ class ZealotWander
 {
 public:
 	ZealotWander(BWAPI::Unit *unit): m_unit(unit), m_connection(Signal::onFrame().connect(boost::bind(&ZealotWander::onFrame, this))),
-						m_visitedPositions() {}
+						m_visitedPositions(), m_attacking(false) {}
 
 	~ZealotWander(void) {m_connection.disconnect();}
 
@@ -14,9 +14,11 @@ public:
 
 private:
 	BWAPI::Position newDestination(void);
+	BWAPI::Unit *nearestEnemy(void);
 
 	BWAPI::Unit *m_unit;
 	boost::signals::connection m_connection;
 
 	std::set<BWAPI::Position> m_visitedPositions;
+	bool m_attacking;
 };
