dd163a50d491ba84318ab770d41b266e83b98f49
RJ Harding <goldgeneralh@gmail.com>
2011-08-23 19:25:39 -0700

    Updated the ResourceManager.cpp to include priorities, added comments to the top describing how to use it.

--- Source/Util/ResourceManager.cpp
+++ Source/Util/ResourceManager.cpp
@@ -14,26 +14,42 @@ ResourceManager::~ResourceManager(void)
 }
 
 //Request the resource manager to allocate the given resources
-int ResourceManager::requestResources(int minerals, int gas)
+int ResourceManager::requestResources(int minerals, int gas, int priority)
 {
 	//make a request and add it to the queue
 	Request newRequest(generateID(), minerals, gas);
-	requests.push(newRequest);
+	switch (priority)
+	{
+	case 1:
+		priorityOne.push(newRequest);
+		break;
+	case 2:
+		priorityTwo.push(newRequest);
+		break;
+	case 3:
+		priorityThree.push(newRequest);
+		break;
+	case 4:
+		priorityFour.push(newRequest);
+		break;
+	default:
+		priorityFive.push(newRequest);
+	}
 	return newRequest.ID;
 }
 
 //Request the resource manager to allocate the resources required to build the count given of the unit type
-int ResourceManager::requestResources(BWAPI::UnitType requestedUnit, int count)
+int ResourceManager::requestResources(BWAPI::UnitType requestedUnit, int count, int priority)
 {
 	//call the more general version of request resources
-	return requestResources(requestedUnit.mineralPrice() * count, requestedUnit.gasPrice() * count);
+	return requestResources(requestedUnit.mineralPrice() * count, requestedUnit.gasPrice() * count, priority);
 }
 
 //Request the resource manager to allocate the resources required to build a unit of this type
-int ResourceManager::requestResources(BWAPI::UnitType requestedUnit)
+int ResourceManager::requestResources(BWAPI::UnitType requestedUnit, int priority)
 {
 	//call the more general version of request resources
-	return requestResources(requestedUnit, 1);
+	return requestResources(requestedUnit, 1, priority);
 }
 
 //Check the 
@@ -45,20 +61,122 @@ void ResourceManager::checkResources()
 	totalMinerals -= allocatedMinerals;
 	totalGas -= allocatedGas;
 	bool done = false;
+
+
+
+	//attempt to approve as many priority one items as possible
 	while (!done)
 	{
-		if (requests.empty()) {
-			break;
-		}
-
-		if ((requests.front().mineralCost <= totalMinerals) && (requests.front().gasCost <= totalGas))
+		//start with priority one items
+		if (!priorityOne.empty())
 		{
-			totalMinerals -= requests.front().mineralCost;
-			allocatedMinerals += requests.front().mineralCost;
-			totalGas -= requests.front().gasCost;
-			allocatedGas += requests.front().gasCost;
-			approved.push_back(Request(requests.front().ID, requests.front().mineralCost, requests.front().gasCost));
-			requests.pop();
+			//When there are priority one items check if they may be approved
+			if ((priorityOne.front().mineralCost <= totalMinerals) && (priorityOne.front().gasCost <= totalGas))
+			{
+				totalMinerals -=priorityOne.front().mineralCost;
+				allocatedMinerals += priorityOne.front().mineralCost;
+				totalGas -= priorityOne.front().gasCost;
+				allocatedGas += priorityOne.front().gasCost;
+				approved.push_back(Request(priorityOne.front().ID, priorityOne.front().mineralCost, priorityOne.front().gasCost));
+				priorityOne.pop();
+			}
+			else
+			{
+				//there are stil priority one items, do not approve anything else until they can be approved
+				return;
+			}
+		}
+		else
+		{
+			//check priority two items
+			if (!priorityTwo.empty())
+			{
+				//when there are priority two items check if they may be approved
+				if ((priorityTwo.front().mineralCost <= totalMinerals) && (priorityTwo.front().gasCost <= totalGas))
+				{
+					totalMinerals -=priorityTwo.front().mineralCost;
+					allocatedMinerals += priorityTwo.front().mineralCost;
+					totalGas -= priorityTwo.front().gasCost;
+					allocatedGas += priorityTwo.front().gasCost;
+					approved.push_back(Request(priorityTwo.front().ID, priorityTwo.front().mineralCost, priorityTwo.front().gasCost));
+					priorityTwo.pop();
+				}
+				else
+				{
+					//there are still priority two items, do not approve anything lower until they can be approved
+					return;
+				}
+			}
+			else
+			{
+				//check priority three items
+				if (!priorityThree.empty())
+				{
+					//when there are priority two items check if they may be approved
+					if ((priorityThree.front().mineralCost <= totalMinerals) && (priorityThree.front().gasCost <= totalGas))
+					{
+						totalMinerals -=priorityThree.front().mineralCost;
+						allocatedMinerals += priorityThree.front().mineralCost;
+						totalGas -= priorityThree.front().gasCost;
+						allocatedGas += priorityThree.front().gasCost;
+						approved.push_back(Request(priorityThree.front().ID, priorityThree.front().mineralCost, priorityThree.front().gasCost));
+						priorityThree.pop();
+					}
+					else
+					{
+						//there are still priority Three items, do not approve anything lower until they can be approved
+						return;
+					}
+				}
+				else
+				{
+					//check priority four items
+					if (!priorityFour.empty())
+					{
+						//when there are priority four items check if they may be approved
+						if ((priorityFour.front().mineralCost <= totalMinerals) && (priorityFour.front().gasCost <= totalGas))
+						{
+							totalMinerals -=priorityFour.front().mineralCost;
+							allocatedMinerals += priorityFour.front().mineralCost;
+							totalGas -= priorityFour.front().gasCost;
+							allocatedGas += priorityFour.front().gasCost;
+							approved.push_back(Request(priorityFour.front().ID, priorityFour.front().mineralCost, priorityFour.front().gasCost));
+							priorityFour.pop();
+						}
+						else
+						{
+							//there are still priority Four items, do not approve anything lower until they can be approved
+							return;
+						}
+					}
+					else
+					{
+						//Check priority five items
+						if (!priorityFive.empty())
+						{
+							//When there are priority five items, check if they may be approved
+							if ((priorityFive.front().mineralCost <= totalMinerals) && (priorityFive.front().gasCost <= totalGas))
+							{
+								totalMinerals -=priorityFive.front().mineralCost;
+								allocatedMinerals += priorityFive.front().mineralCost;
+								totalGas -= priorityFive.front().gasCost;
+								allocatedGas += priorityFive.front().gasCost;
+								approved.push_back(Request(priorityFive.front().ID, priorityFive.front().mineralCost, priorityFive.front().gasCost));
+								priorityFive.pop();
+							}
+							else
+							{
+								//there are still priority Five items, do not approve anything lower until they can be approved
+								return;
+							}
+						}
+						else
+						{
+							return; //there are no longer any items
+						}
+					}
+				}
+			}
 		}
 	}
 }
@@ -73,7 +191,7 @@ int ResourceManager::generateID()
 //check if a request has been approved
 bool ResourceManager::checkReady(int ID)
 {
-	for (int i = 0; i < approved.size(); i++)
+	for (unsigned int i = 0; i < approved.size(); i++)
 	{
 		if (ID == approved[i].ID)
 		{
@@ -85,7 +203,7 @@ bool ResourceManager::checkReady(int ID)
 
 void ResourceManager::resourcesUsed(int ID)
 {
-	for (int i = 0; i < approved.size(); i++)
+	for (unsigned int i = 0; i < approved.size(); i++)
 	{
 		if (ID == approved[i].ID)
 		{
--- Source/Util/ResourceManager.h
+++ Source/Util/ResourceManager.h
@@ -1,3 +1,16 @@
+//This is a resource manager for all tasks that we perform. It is imperitive for this manager to be effective that it is used
+//in every instance where we need resources. 
+
+//The procedure is: When some part of the AI wants to build something...
+//Call the requestResources function and give it the number of resources needed(or a unit type and number of requested units)
+//as well as a priority so the resource manger knows how important the request is.
+//The requestResources function will return an integer wich is your callback number.
+//When you want to check if your request has been approved call check ready and give your callback number.
+//After your request has been approved, please use the resourcesUsed function immedately after you use the resources so
+//the manager will know that it doesnt need to hold those resources anymore.
+//FREEING UP RESOURCES WITH RESOURCESUSED IS VERY IMPORTANT!!!
+
+
 #pragma once
 
 #include <BWAPI.h>
@@ -14,9 +27,9 @@ public:
 	~ResourceManager(void);
 
 	//Request the resources manager to allocate the given amount of resources
-	int requestResources(int minerals, int gas); 
-	int requestResources(BWAPI::UnitType requestedUnit, int count);
-	int requestResources(BWAPI::UnitType requestedUnit);
+	int requestResources(int minerals, int gas, int priority); 
+	int requestResources(BWAPI::UnitType requestedUnit, int count, int priority);
+	int requestResources(BWAPI::UnitType requestedUnit, int priority);
 
 	bool checkReady(int ID); //give your number and this function returns true if your request is ready
 	void resourcesUsed(int ID); //when your request is ready, use the resources allocated and then call this function giving your number
@@ -24,13 +37,12 @@ public:
 	int minerals(void) {return Broodwar->self()->minerals() - allocatedMinerals;}
 	int gas(void) {return Broodwar->self()->gas() - allocatedGas;}
 private:
-
+	//Structure wich represents a request for resources
 	struct Request
 	{
 		Request(int inID, int inmineralCost, int ingasCost):ID(inID),
 															mineralCost(inmineralCost),
 															gasCost(ingasCost) {}
-
 		int ID;
 		int mineralCost;
 		int gasCost;
@@ -41,7 +53,11 @@ private:
 	int allocatedMinerals; //total minerals that have been reserved
 	int allocatedGas; //total gas that has been reserved
 
-	queue<Request> requests; //active requests
+	queue<Request> priorityOne; //highest priority
+	queue<Request> priorityTwo; //second
+	queue<Request> priorityThree; //third
+	queue<Request> priorityFour; //fourth
+	queue<Request> priorityFive; //fifth
 	vector<Request> approved; //approved requests
 	void checkResources(); //during every frame check if there are enough resources for the top of the queue
 };
