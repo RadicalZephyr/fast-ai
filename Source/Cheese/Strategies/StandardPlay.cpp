b40bb427fb7dfc8c989ed6619a44f078771132ed
Jeremy Jusak <jjusak@comcast.net>
2011-09-01 21:04:21 -0700

    Added methods in StandardPlay to check for important upgrades and tech buildings. These methods are currently called on frame.
    Currently using Total Supply conditions for Buildings, may need improvement.
    
    Signed-off-by: Jeremy Jusak <jjusak@comcast.net>

--- Source/Cheese/Strategies/StandardPlay.cpp
+++ Source/Cheese/Strategies/StandardPlay.cpp
@@ -2,7 +2,8 @@
 #include "BaseManager\BaseManager.h"
 
 void CheeseStrategies::StandardPlay::onFrame() {
-	
+	upgradeChecker();
+	techBuildingChecker();
 }
 
 void CheeseStrategies::StandardPlay::init() {
@@ -23,4 +24,86 @@ void CheeseStrategies::StandardPlay::startBuilding() {
 	m_base->constructBuilding(BWAPI::UnitTypes::Protoss_Cybernetics_Core);
 	m_base->constructBuilding(BWAPI::UnitTypes::Protoss_Pylon);
 	m_base->constructBuilding(BWAPI::UnitTypes::Protoss_Gateway);
+}
+
+bool CheeseStrategies::StandardPlay::startUpgrade(BWAPI::UpgradeType upgrade) {
+	UnitType upBuildingType = upgrade.whatUpgrades();
+
+	if (Broodwar->self()->completedUnitCount(upBuildingType) == 0) {
+		return false;
+	}
+
+	UnitSet myUnits = BWAPI::Broodwar->self()->getUnits();
+	UnitSet::iterator it;
+
+	BWAPI::Unit* upBuilding;
+
+	for (it=myUnits.begin(); it!=myUnits.end(); it++) {
+		if ((*it)->getType() == upBuildingType) {
+			upBuilding = (*it);
+		}
+	}
+
+	return upBuilding->upgrade(upgrade);
+}
+
+bool CheeseStrategies::StandardPlay::startTech(BWAPI::TechType tech) {
+	UnitType techBuildingType = tech.whatResearches();
+
+	if (Broodwar->self()->completedUnitCount(techBuildingType) == 0) {
+		return false;
+	}
+
+	UnitSet myUnits = BWAPI::Broodwar->self()->getUnits();
+	UnitSet::iterator it;
+
+	BWAPI::Unit* techBuilding;
+
+	for (it=myUnits.begin(); it!=myUnits.end(); it++) {
+		if ((*it)->getType() == techBuildingType) {
+			techBuilding = (*it);
+		}
+	}
+
+	return techBuilding->research(tech);
+}
+
+void CheeseStrategies::StandardPlay::upgradeChecker() {
+	if (!dragoonRange) {
+		dragoonRange = startUpgrade(BWAPI::UpgradeTypes::Singularity_Charge);
+	}
+	if (!zealotLegs) {
+		zealotLegs = startUpgrade(BWAPI::UpgradeTypes::Leg_Enhancements);
+	}
+	if (!psiStorm) {
+		psiStorm = startTech(BWAPI::TechTypes::Psionic_Storm);
+	}
+	if (!kAmulet && psiStorm) {
+		kAmulet = startUpgrade(BWAPI::UpgradeTypes::Khaydarin_Amulet);
+	}
+}
+
+void CheeseStrategies::StandardPlay::techBuildingChecker() {
+	if (!citadel && 
+		Broodwar->self()->completedUnitCount(BWAPI::UnitTypes::Protoss_Cybernetics_Core) >= 1 && 
+		Broodwar->self()->supplyUsed() >= 65) { 
+
+		citadel = m_base->constructBuilding(BWAPI::UnitTypes::Protoss_Citadel_of_Adun);
+	}
+	if (!robotics && 
+		Broodwar->self()->completedUnitCount(BWAPI::UnitTypes::Protoss_Cybernetics_Core) >= 1 && 
+		Broodwar->self()->supplyUsed() >= 50) { 
+
+		robotics = m_base->constructBuilding(BWAPI::UnitTypes::Protoss_Robotics_Facility);
+	}
+	if (!observatory && 
+		Broodwar->self()->completedUnitCount(BWAPI::UnitTypes::Protoss_Robotics_Facility) >= 1) { 
+
+		observatory = m_base->constructBuilding(BWAPI::UnitTypes::Protoss_Observatory);
+	}
+	if (!templarArchives && 
+		Broodwar->self()->completedUnitCount(BWAPI::UnitTypes::Protoss_Citadel_of_Adun) >= 1) { 
+
+		templarArchives = m_base->constructBuilding(BWAPI::UnitTypes::Protoss_Templar_Archives);
+	}
 }
--- Source/Cheese/Strategies/StandardPlay.h
+++ Source/Cheese/Strategies/StandardPlay.h
@@ -10,19 +10,39 @@ public:
 		m_frameConnection = Signal::onFrame().connect(boost::bind(&StandardPlay::onFrame, this));
 		whichProbe = 6;
 		buildProbesTo = -1;
+
+		dragoonRange = false;
+		zealotLegs = false;
+		psiStorm = false;
+		kAmulet = false;
 	}
 
 	virtual void onFrame();
 	virtual void init();
 	
 	void startBuilding();
+	bool startUpgrade(BWAPI::UpgradeType upgrade);
+	bool startTech(BWAPI::TechType tech);
+	void upgradeChecker();
+	void techBuildingChecker();
 
 private:
+	boost::signals::connection m_frameConnection;
+
 	BWAPI::Unit* m_probe;
 	BaseManagerPtr m_base;
 
-	boost::signals::connection m_frameConnection;
-	
+	bool dragoonRange;
+	bool zealotLegs;
+	bool psiStorm;
+	bool kAmulet;
+
+	bool citadel;
+	bool templarArchives;
+	bool robotics;
+	bool roboticsBay;
+	bool observatory;
+	bool stargate;
 };
 
 }
